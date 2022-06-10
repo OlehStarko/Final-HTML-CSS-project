0737db08c65e9625866debbc8752aefd13e84623
OlehStarko <100271805+OlehStarko@users.noreply.github.com>
2022-06-10 19:56:11 +0000

    Deploying to gh-pages from @ OlehStarko/final-HTML-CSS-project@e4b02af07371572276ba0a67c03c05b84fbb4462 🚀

---
+++ index.03fdd53c.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector(".tel-menu-open"),closeMenuBtn:document.querySelector(".tel-menu-close"),menu:document.querySelector(".tel-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.03fdd53c.js.map
