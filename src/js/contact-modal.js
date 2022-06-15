358cb1b66886b740239ab244d92ea455a7535c8c
OlehStarko <starartds@gmail.com>
2022-06-15 18:46:19 +0300

    edit-mail

--- src/js/contact-modal.js
+++ src/js/contact-modal.js
@@ -6,8 +6,12 @@ const modalclose = document.querySelector('.modal-close');
 const modalclose1 = document.querySelector('.modal-close1');
 const modalclose2 = document.querySelector('.modal-close2');
 const modalbtn = document.querySelector('.contact-button__margin');
+const contacttel = document.querySelector('.contact-tel');
+const contactmail = document.querySelector('.contact-mail');
 console.log(children);
 console.log(contactcard1);
+console.log(contactmail);
+
 for (let i = 0; i < children.length; i += 1) {
   if (i === 0) {
     children[i].addEventListener('click', function () {
@@ -18,6 +22,7 @@ for (let i = 0; i < children.length; i += 1) {
       document.body.style.position = 'fixed';
     });
   }
+
   if (i === 1) {
     children[i].addEventListener('click', function () {
       contactcard2.classList.toggle('card-visible');
@@ -44,12 +49,14 @@ modalclose.addEventListener('click', function () {
   document.body.style.position = 'static';
   window.scrollTo(0, parseInt(scrollY) * -1);
 });
+
 modalclose1.addEventListener('click', function () {
   contactcard2.classList.toggle('card-visible');
   const scrollY = document.body.style.top;
   document.body.style.position = 'static';
   window.scrollTo(0, parseInt(scrollY) * -1);
 });
+
 modalclose2.addEventListener('click', function () {
   contactcard3.classList.toggle('card-visible');
   const scrollY = document.body.style.top;
@@ -63,4 +70,11 @@ modalbtn.addEventListener('click', function () {
   document.body.style.right = 0;
   document.body.style.left = 0;
   document.body.style.position = 'fixed';
+});
+
+contacttel.addEventListener('click', function () {
+  contactcard1.classList.toggle('card-hidden');
+});
+contactmail.addEventListener('click', function () {
+  contactcard1.classList.toggle('card-hidden');
 });
