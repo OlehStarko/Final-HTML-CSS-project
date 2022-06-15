bd504d31eb2df063c4b2f424d3b6c88e612f1f46
OlehStarko <starartds@gmail.com>
2022-06-15 10:37:20 +0300

    edit modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,21 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+const openBackdropTel = document.querySelector('.backdrop-tel');
+const openModalTel = document.querySelector('.tel-modal--open');
+console.log(openModalTel);
+openModalTel.addEventListener('click', function () {
+  openBackdropTel.classList.toggle('is-hidden');
+});
