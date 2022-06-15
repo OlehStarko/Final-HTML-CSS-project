d0b55d5324af45ef20796fa03849a35426d50cea
OlehStarko <100271805+OlehStarko@users.noreply.github.com>
2022-06-15 07:38:00 +0000

    Deploying to gh-pages from @ OlehStarko/final-HTML-CSS-project@bd504d31eb2df063c4b2f424d3b6c88e612f1f46 🚀

---
+++ index.6657d01a.js
@@ -0,0 +1,2 @@
+!function(){!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();var e=document.querySelector(".backdrop-tel"),o=document.querySelector(".tel-modal--open");console.log(o),o.addEventListener("click",(function(){e.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.6657d01a.js.map
